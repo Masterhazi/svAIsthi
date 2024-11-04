20aa2e375d74cf243244c1aec7561da0b1fd8bbe
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-11-04 23:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ youtube_api_key = os.getenv("YOUTUBE_API_KEY")
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 # Configuring the page
-st.set_page_config(page_title="💊 svAIsthi", page_icon="https://github.com/Masterhazi/svAIsthi/blob/main/health-8.svg")
+st.set_page_config(page_title="💊 svAIsthi", page_icon="https://github.com/Masterhazi/svAIsthi/blob/main/health-8.ico")
 st.title('svAIsthi - Svasth with AI')
 st.caption('Your AI assistant for health education and help')
 
