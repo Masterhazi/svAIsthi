03d9827cfe078949c93a94731d375601d2d8f6b9
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-11-28 17:33:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+selector = st.Page(page = 'choice.py', title = 'Login/Sign up page' )
+login = st.Page(page = 'Login.py', title = 'Login page')
+register = st.Page(page = 'Register.py', title = 'Register now')
+home = st.Page(page = 'home.py', titile = 'Ra ra Bhattu raa')
+
