f4de1fd2ebe85dfe9d4291115c3fb7ccf83c8e6e
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-11-28 11:41:25 +0530

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="svAIsthi - Home", page_icon="💊")
+
+st.title("Welcome to svAIsthi")
+st.caption("Choose your action below:")
+
+# Navigation buttons
+if st.button("Log In"):
+    st.session_state["current_page"] = "Login"
+    st.experimental_rerun()
+
+if st.button("Register"):
+    st.session_state["current_page"] = "Register"
+    st.experimental_rerun()
